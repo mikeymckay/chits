0911b87119048299398785fc76eac0fcc64b4e0b
JVTolentino <jvtolentino@gmail.com>
2009-10-07 10:06:39 +0800

    class.dental.php with unknown errors on set_detail / dhcmain commit 1

---
+++ modules/dental/class.dental.php
@@ -0,0 +1,96 @@
+<?
+  class dental extends module {
+    
+    // Author: Herman Tolentino MD
+    // CHITS Project 2004
+            
+    // DENTAL HEALTH CARE PROGRAM MODULE
+                           
+    // Feel free to add additional comments anywhere.
+    // Just add comment dates before the actual comment.
+                                                                      
+    // COMMENT DATE: Sep 25, '09
+    // THESE ARE THE REQUIRED APIs/FUNCTIONS FOR EVERY MODULE
+    // 1. init_deps()
+    // 2. init_lang()
+    // 3. init_stats()
+    // 4. init_help()
+    // 5. init_menu()
+    // 6. init_sql()
+    // 7. CONSTRUCTOR FUNCTION
+    // 8. drop_tables()
+                                                                                        
+                                                                                          
+    // Comment date: Sep 25, '09
+    // The constructor function starts here
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    function dental_health_care() {
+      $this->author = 'Jeffrey V. Tolentino';
+      $this->version = '0.1'.date("Y-m-d");
+      $this->module = 'dental';
+      $this->description = 'CHITS Module - Dental Health Care Program';
+    }
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                                                                                                        
+                                                                                                                                          
+    function init_deps() {
+    }
+                                                                                                                                                    
+                                                                                                                                                      
+    function init_lang() {
+    }	
+                                                                                                                                                                
+                                                                                                                                                                  
+    function init_stats() {
+    }
+                                                                                                                                                                            
+                                                                                                                                                                              
+    function init_help() {
+    }
+                                                                                                                                                                                      
+                                                                                                                                                                                      
+    // Comment date: Sep 25, '09
+    // The init_menu() function starts here
+    // This function is used to include a link to the menu pane,
+    // to the pane at the bottom of the menu pane, and so on...
+    // (The menu pane is located at the left side of the site)
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    function init_menu() {
+      if (func_num_args()>0) {
+        $arg_list = func_get_args();
+      }
+                                                                                                                                                                                                                                      
+      // set_menu parameters
+      // set_menu([module name], [menu title - what is displayed], menu categories (top menu)], [script executed in class])
+      module::set_menu($this->module, "Dental Records", "PATIENTS", "_dental");
+      // set_menu parameters
+      // set_menu([module description], [module version], [module author], [module name/id]
+      module::set_detail($this->description, $this->version, $this->author, $this->module);
+    }
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                          
+    function init_sql() {
+    }
+                                                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                                                                                                          
+    function drop_tables() {
+    }
+                                                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                                                      
+    function _dental() {
+    
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // IMPORTANT:
+    //	THE CODE FOR THIS FUNCTION IS IN A SEPARATE FILE. (NAME dhcmain.php, 
+    //  LOCATED AT chits_modules_factory, my working directory for chits modules).
+    //  I WILL APPEND THE CONTENT OF THE FILE LATER HERE.
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    
+    echo "Hello World";
+    
+    }
+     
+                  
+  } // class ends here
+?>
---
+++ modules/dental/dhcmain.php
@@ -0,0 +1,462 @@
+<?
+
+  // Comment date: Oct 6, '09
+  // The following function will be used for acquiring tooth condition,
+  // the condition will be retrieved from db later.
+  // Delete and replace this comment if data are being retrieved from db.
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  function tooth_condition($tn) {
+    return 'Y'.$tn;
+  }
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  // Comment date: Oct 6, '09
+  // The following functions will be used for acquiring the patient's
+  //   personal info from a db.
+  // Delete and replace this comment if data are being retrieved from db.
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  function patient_name($p_id) {
+    return 'Jeffrey V. Tolentino';
+  }
+  
+  function patient_birthdate($p_id) {
+    return 'July 27, 1991';
+  }
+  
+  function patient_age($p_id) {
+    return '18';
+  }
+  
+  function patient_sex($p_id) {
+    return 'Male';
+  }
+  
+  function patient_place_of_birth($p_id) {
+    return "find out if this is included in the patient's info in CHITS.";
+  }
+  
+  function patient_address($p_id) {
+    return 'Gerona, Tarlac';
+  }
+  
+  function patient_occupation($p_id) {
+    return 'dunno';
+  }
+  
+  function patient_parent($p_id) {
+    return 'none';
+  }
+  
+  function patient_medical_history($p_id) {
+    return "must find a way to link this to patient's medical history.";
+  }
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  
+  echo "<p>&nbsp</p>";
+  
+  
+  // Comment date: Oct 6, '09
+  // The following codes will be used to output to the 
+  //    screen the patient's personal info.
+  // These data are based on my interview with Dr Leandro Domingo
+  //    (Dental Health Coordinator - PHO) on Sep 24, '09.
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  $patient_id = '1';
+  $patient_name = patient_name($patient_id);
+  $patient_birthdate = patient_birthdate($patient_id);
+  $patient_place_of_birth = patient_place_of_birth($patient_id);
+  $patient_age = patient_age($patient_id);
+  $patient_sex = patient_sex($patient_id);
+  $patient_address = patient_address($patient_id);
+  $patient_occupation = patient_occupation($patient_id);
+  $patient_parent = patient_parent($patient_id);
+  $patient_medical_history = patient_medical_history($patient_id);
+  
+  echo "<table border=1 cellspacing=1 align='center' width=600>";
+    echo "<tr>";
+      echo "<th>Individual Patient Treatment Record</th>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Patient ID:</i> $patient_id</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Name:</i> $patient_name</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Date of Birth:</i> $patient_birthdate</td>";
+    //  echo "<td><i>Age:</i> $patient_age</td>";
+    //  echo "<td><i>Sex:</i> $patient_sex</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+    //  echo "<td><i>Date of Birth:</i> $patient_birthdate</td>";
+      echo "<td><i>Age:</i> $patient_age</td>";
+    //  echo "<td><i>Sex:</i> $patient_sex</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+    //  echo "<td><i>Date of Birth:</i> $patient_birthdate</td>";
+    //  echo "<td><i>Age:</i> $patient_age</td>";
+      echo "<td><i>Sex:</i> $patient_sex</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Place of Birth:</i> $patient_place_of_birth</td>";
+    echo "</td>";
+    
+    echo "<tr>";
+      echo "<td><i>Address:</i> $patient_address</td";    
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Occupation:</i> $patient_occupation</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Parent/Guardian:</i> $patient_parent</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td><i>Medical History:</i> $patient_medical_history</td>";
+    echo "</tr>";
+  
+  echo "</table>";
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  
+  // Comment date: Oct 6, '09
+  // The following codes will be used to output to the 
+  //    screen the patient's Oral Health Condition (A), 
+  //    and the ways the user can update/modify it.
+  // These data are based on my interview with Dr Leandro Domingo
+  //    (Dental Health Coordinator - PHO) on Sep 24, '09.
+  
+  // Comment date: Oct 6, '09 (16:15)
+  // Check wether the user have to modify these
+  //    data. It seems these data can be acquired when the dentist
+  //    fills-up his/her Oral Health Condition chart(?).
+  // The chart is located at the back of the photocopied Individual
+  //    Treatment Record given by Dr Domingo.
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  echo "<p>&nbsp</p>";
+      
+  // Oral Health Conditon (A)
+  echo "<table border=1 align='center' width=600>";
+    echo "<tr>";
+      echo "<th align='left'>Oral Health Condition (A)</th>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Date of Oral Examination: ".
+        "<input type='textbox' size=10></input>".
+        "<i> (mm-dd-yyyy)</i>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Check the box on the left if condition is present on patient</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Dental Caries</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Gingivitis/Periodontal Disease</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Debris</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Calculus</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Abnormal Growth</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Cleft Lip/Palate</input>".
+        "</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>".
+        "<input type='checkbox'>Others (supernumerary/mesiodens, etc)</input>".
+        "</td>";
+    echo "</tr>";
+    
+  echo "</table>";
+    
+    
+  // Oral Health Condition (B)
+  echo "<table border=1 align='center' width='600'>";
+    
+    echo "<tr>";
+      echo "<th align='left'>Oral Health Condition (B)</th>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Indicate number on the right side of the condition</td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Permanent Teeth Present</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Permanent Sound Teeth</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Decayed Teeth (D)</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Missing Teeth (M)</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Filled Teeth (F)</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Total DMF Teeth</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Temporary Teeth Present</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of Temporary Sound Teeth</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of decayed teeth (d)</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Number of filled teeth (f)</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";
+    
+    echo "<tr>";
+      echo "<td>Total df Teeth</td>";
+      echo "<td><input type='textbox' size=5></input></td>";
+    echo "</tr>";     
+  
+  echo "</table>";
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  
+  
+  // Comment date: 0ct 6, '09
+  // The following codes will be used to initialize the patient's
+  // teeth conditions. The teeth and condition symbols are based on
+  // information gathered during my interview with Dr Leandro Domingo
+  //    (Dental Health Coordinator - PHO) on Sep 24, '09.
+  // 	Permanent					Temporary
+  // 	Legend		Tooth Condition			Legend
+  //	Y		Sound/Sealed			y	(the original symbol is the check symbol, if possible, use it)
+  //	D		Decayed				d
+  //	F		Filled				f
+  //	M		Missing				e
+  //	X		Indicated for extraction	x
+  //	Un		Unerupted			un
+  //	S		Supernumerary tooth		s
+  //	JC		Jacket Crown			jc
+  //	P		Pontic				p
+  //	
+  //	Legend:
+  //	S - Sealant
+  //	PF - Permanent Filling (Composite, ??) ?? bad data due to poor photocopy
+  //	TF - Temporary Filling
+  // 	X - Extraction
+  //	O - Others
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  echo "<p>&nbsp</p>";
+  $toothnumber = 0;
+  $condition[$toothnumber] = 'Y';
+  
+  // upper-teeth (temporary)
+  for($toothnumber=51; $toothnumber<=55; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  for($toothnumber=61; $toothnumber<=65; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  
+  // upper-teeth (permanent)
+  for($toothnumber=11; $toothnumber<=18; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  for($toothnumber=21; $toothnumber<=28; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  
+  // lower-teeth (permanent)
+  for($toothnumber=41; $toothnumber<=48; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  for($toothnumber=31; $toothnumber<=38; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  
+  // lower-teeth (temporary)
+  for($toothnumber=81; $toothnumber<=85; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  for($toothnumber=71; $toothnumber<=75; $toothnumber++) {
+    $condition[$toothnumber] = tooth_condition($toothnumber);
+  }
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  
+  
+  // Comment date: Oct 6, '09
+  // The following codes are used to populate a table with
+  //   teeth symbols and conditions.
+  // My initial plan is to refresh the page every
+  //   tooth condition updates.
+  // Will see if this is acceptable, especially
+  //   when querying the db.
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  
+  // upper-teeth-temporary symbols and conditions
+  echo "<table border=1 align='center' width=600>";
+    echo "<tr>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      for($toothnumber=55; $toothnumber>=51; $toothnumber--) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      for($toothnumber=61; $toothnumber<=65; $toothnumber++) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+    echo "</tr>";
+  
+    echo "<tr>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      for($toothnumber=55; $toothnumber>=51; $toothnumber--) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      for($toothnumber=61; $toothnumber<=65; $toothnumber++) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+    echo "</tr>";
+  
+  
+    // upper-teeth-permanent symbols and conditions
+    echo "<tr>";
+      for($toothnumber=18; $toothnumber>=11; $toothnumber--) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      for($toothnumber=21; $toothnumber<=28; $toothnumber++) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+    echo "</tr>";
+  
+    echo "<tr>";
+      for($toothnumber=18; $toothnumber>=11; $toothnumber--) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      for($toothnumber=21; $toothnumber<=28; $toothnumber++) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+    echo "</tr>";
+  echo "</table>";
+  
+  echo "<p>&nbsp</p>";
+  
+  
+  // lower-teeth-permanent symbols and conditions
+  echo "<table border=1 align='center'>";
+    echo "<tr>";
+      for($toothnumber=48; $toothnumber>=41; $toothnumber--) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      for($toothnumber=31; $toothnumber<=38; $toothnumber++) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+    echo "</tr>";
+  
+    echo "<tr>";
+      for($toothnumber=48; $toothnumber>=41; $toothnumber--) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      for($toothnumber=31; $toothnumber<=38; $toothnumber++) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+    echo "</tr>";
+  
+  
+    // lower-teeth-temporary symbols and conditions
+    echo "<tr>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      for($toothnumber=85; $toothnumber>=81; $toothnumber--) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      for($toothnumber=71; $toothnumber<=75; $toothnumber++) {
+        echo "<td align='center'>$condition[$toothnumber]</td>";
+      }
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+    echo "</tr>";
+  
+    echo "<tr>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      for($toothnumber=85; $toothnumber>=81; $toothnumber--) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      for($toothnumber=71; $toothnumber<=75; $toothnumber++) {
+        echo "<td align='center'><b>$toothnumber</b></td>";
+      }
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+      echo "<td align='center'></td>";
+    echo "</tr>";
+  echo "</table>";
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+?>
