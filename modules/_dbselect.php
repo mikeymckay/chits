20208fa583be74a4da21f90364dfb547c2a0e5f6
mikeymckay <mikeymckay@gmail.com>
2010-01-13 14:26:22 -0500

    _dbselect.php now has default settings for developers but is changed when doing a deployment by tarlac_install.sh

--- modules/_dbselect.php
+++ modules/_dbselect.php
@@ -1,9 +1,11 @@
 <?php
-// Rename this file to _dbselect.php and update the values below to match your own database username/password
+// 
+// *** DO NOT use the chits_development database for real world deployments. Use chits_live with a secure password.
+//
 session_start();
-$_SESSION["dbname"] = "chits"; // Name of the database (probably chits)
-$_SESSION["dbuser"] = "root"; // mysql username that you are using to connect to the database
-$_SESSION["dbpass"] = "root"; // mysql password that you are using to connect to the database
+$_SESSION["dbname"] = "chits_development"; // Name of the mysql database (try "SHOW DATABASES;" on the mysql console
+$_SESSION["dbuser"] = "chits_developer"; // mysql username that you are using to connect to the database
+$_SESSION["dbpass"] = "password"; // mysql password that you are using to connect to the database
 $conn = mysql_connect("localhost", $_SESSION["dbuser"], $_SESSION["dbpass"]);
 $db->connectdb($_SESSION["dbname"]);
 ?>
